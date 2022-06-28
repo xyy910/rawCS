c518b90c6c2d83cecc9427c6531c5c062434eed1
yang.zheng <yang.zheng@ly.com>
2022-06-29 02:33:16 +0800

    aaa

---
+++ client/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"log"
+	"math/rand"
+	"net"
+	"time"
+)
+
+func IntToBytes(n int) []byte {
+	x := int32(n)
+
+	bytesBuffer := bytes.NewBuffer([]byte{})
+	binary.Write(bytesBuffer, binary.BigEndian, x)
+	return bytesBuffer.Bytes()
+}
+
+func main() {
+	tcpAddr, err := net.ResolveTCPAddr("tcp4", "127.0.0.1:8090")
+	if err != nil {
+		log.Fatalln("第一步就出错啦！", err)
+	}
+	conn, err := net.DialTCP("tcp4", nil, tcpAddr)
+	if err != nil {
+		log.Fatalln("建立连接出错啦：", err)
+	}
+
+	//fmt.Println("发送字符串：时代少年团-渐暖")
+	//n, err := conn.Write([]byte("时代少年团-渐暖"))
+	//if err != nil {
+	//	log.Fatalln("发送出错啦!", err)
+	//}
+	//fmt.Println("客户端发送了：", n)
+
+	var buf []byte
+	fmt.Println("连续发整数1~100")
+	for i := 1; i <= 10; i++ {
+		buf = append(buf, IntToBytes(i)...)
+	}
+	for i :=0; i<len(buf); i++ {
+		conn.Write(buf[i:i+1])
+		time.Sleep(time.Millisecond * time.Duration(rand.Intn(20)))
+	}
+
+}
---
+++ server/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"io"
+	"log"
+	"net"
+)
+
+func printShow(conn *net.TCPConn) {
+	buff := make([]byte, 1024)
+	changdu, err := conn.Read(buff)
+	var res []byte
+	for changdu > 0 && err == nil {
+		fmt.Println("收到了", changdu, "err是：", err)
+		res = append(res, buff[0:changdu]...)
+		xianyoude := len(res)
+		if xianyoude/4 > 0{
+			var res1 []int
+			i := 0
+			for ; i<xianyoude/4; i++ {
+				res1 = append(res1, BytesToInt(res[i*4:(i+1)*4]))
+			}
+			res = res[i*4:]
+			fmt.Println("结果是：", res1)
+			changdu, err = conn.Read(buff)
+		}
+	}
+	if err == io.EOF {
+		fmt.Println("碰到EOF了 到底了", err)
+	}
+
+	fmt.Println("长度不大于0了, err 是： ", err)
+
+}
+
+func BytesToInt(b []byte) int {
+	bytesBuffer := bytes.NewBuffer(b)
+	var x int32
+	binary.Read(bytesBuffer, binary.BigEndian, &x)
+	return int(x)
+}
+
+func main() {
+	address := net.TCPAddr{
+		IP:   net.ParseIP("127.0.0.1"),
+		Port: 8090,
+	}
+	listener, err := net.ListenTCP("tcp4", &address)
+	if err != nil {
+		log.Fatalln("监听出错啦：", err)
+	}
+	for {
+		conn, err := listener.AcceptTCP()
+		if err != nil {
+			log.Fatalln("accept出错啦：", err)
+		}
+		fmt.Println("咦！有个臭活现上钩了！", conn.RemoteAddr())
+		go printShow(conn)
+	}
+}
